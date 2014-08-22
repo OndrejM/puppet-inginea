b4b6b83c345c66258bcde5d8cf8558ee426c6b09
OndrejM <ondrej.mihalyi@gmail.com>
2014-08-22 16:12:21 +0200

    Working apache configuration with origami.inginea.eu virtual server targeting /var/www/origami.inginea.eu/web directory

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -1,7 +1,31 @@
-class { 'apache':  }
+class { 'apache':  
+  mpm_module => 'prefork'
+}
+
+class {'::apache::mod::php':
+}
 
 apache::vhost { 'origami.inginea.eu':
   port    => '80',
   docroot => '/var/www/origami.inginea.eu/web',
+  servername => 'origami.inginea.eu',
+  serveraliases => ['www.origami.inginea.eu'],
+  directories => [
+    { 'path' => '/var/www/origami.inginea.eu/web',
+      'provider' => 'direcotry',
+      'auth_require' => 'all granted' 
+    },
+    { 'path' => '/var/www/origami.inginea.eu/web',
+      'provider' => 'direcotry',
+      'auth_require' => 'all granted' 
+    }
+  ],
+  custom_fragment => 'AddType application/x-httpd-php .php .php3 .php4 .php5',
+  php_admin_values => [
+    'sendmail_path "/usr/sbin/sendmail -t -i -fwebmaster@origami.inginea.eu"',
+    'upload_tmp_dir /var/www/origami.inginea.eu/web/tmp',
+    'session.save_path /var/www/origami.inginea.eu/web/tmp',
+    'open_basedir /var/www/origami.inginea.eu/web:/var/www/origami.inginea.eu/web/private:/var/www/origami.inginea.eu/web/tmp:/srv/www/origami.inginea.eu/web:/usr/share/php5:/usr/share/php:/tmp:/usr/share/phpmyadmin:/etc/phpmyadmin:/var/lib/phpmyadmin'
+  ]
 }
 
