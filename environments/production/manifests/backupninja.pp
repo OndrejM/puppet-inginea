8c208f3f029c89aea271bcb54a3dfdd999b5dc96
Ondrej Mihalyi <ondrej.mihalyi@gmail.com>
2015-08-06 19:20:56 +0200

    Fixing dependencies on backup folders. Copy mysql backups to staging dir

--- environments/production/manifests/backupninja.pp
+++ environments/production/manifests/backupninja.pp
@@ -2,20 +2,45 @@ class {'backupninja':
   ensure => 'present'
 }
 
-backupninja::mysql { '11-backup_mysql-server':
+backupninja::mysql { '11_backup_mysql_server':
   ensure => present,
   compress => no
 }
 
-file {'/home/backup/staging/www/inginea/var/www':
+file {[
+  '/home/backup/staging/mysql/var',
+  '/home/backup/staging/mysql/var/backups',
+  '/home/backup/staging/mysql/var/backups/mysql',
+  '/home/backup/staging/mysql/var/backups/mysql/sqldump']:
   ensure => directory,
-  mode   => 0755
+  mode   => 0755,
+  owner  => backup,
+  group => backup
 }
 
-backupninja::rsync { '12-backup_www_rsync':
+backupninja::rsync { '12_stage_mysql_backups':
   ensure              => 'present',
-  mountpoint          => '/home/backup/staging/www',
-  backupdir           => 'inginea',
+  mountpoint          => '/home/backup/staging',
+  backupdir           => 'mysql',
+  source_type         => 'local',
+  source_include      => 'var/backups/mysql/sqldump',
+  dest_type           => 'local',
+  format              => 'mirror'
+}
+
+file {[
+  '/home/backup/staging/www/var',
+  '/home/backup/staging/www/var/www']:
+  ensure => directory,
+  mode   => 0755,
+  owner  => backup,
+  group => backup
+}
+
+backupninja::rsync { '13_backup_www_rsync':
+  ensure              => 'present',
+  mountpoint          => '/home/backup/staging',
+  backupdir           => 'www',
   source_type         => 'local',
   source_include      => 'var/www',
   dest_type           => 'local',
